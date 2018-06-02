9624184307f6e256dbb1899099c8c52aaccabba2
Eunice wairimu <eunicewairimu227@gmail.com>
2018-06-02 12:58:11 +0300

    adding dict file

---
+++ dict.py
@@ -0,0 +1,6 @@
+Python 3.7.0b4 (v3.7.0b4:eb96c37699, May  2 2018, 18:05:41) [MSC v.1913 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> my_dict={'Eunice':'ken','roy':'nyambu'}
+>>> print(my_dict)
+{'Eunice': 'ken', 'roy': 'nyambu'}
+>>> 
